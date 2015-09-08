ae8df9c2314388f6098161a4fef40052a2c98209
joshchandler88 <joshchandler88@gmail.com>
2015-09-08 11:31:33 -0400

    Change wsgi.py

--- musetic/wsgi.py
+++ musetic/wsgi.py
@@ -10,12 +10,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 from musetic.settings.utils import get_env_variable
 
+django_settings = get_env_variable('DJANGO_SETTINGS_MODULE')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", get_env_variable('DJANGO_SETTINGS_MODULE'))
 
 from django.core.wsgi import get_wsgi_application
 
-if get_env_variable('DJANGO_SETTINGS_MODULE') === 'musetic.settings.production':
-  from dj_static import Cling
-  application = Cling(get_wsgi_application())
-else:
-  application = get_wsgi_application()
+application = get_wsgi_application()

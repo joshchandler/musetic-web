c292ef0e343e9c945ba74b655365f845b1375897
joshchandler88 <joshchandler88@gmail.com>
2015-09-14 08:47:27 -0400

    Codeship staging settings

--- musetic/settings/codeship.py
+++ musetic/settings/codeship.py
@@ -5,7 +5,7 @@ from .base import *
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 
-SECRET_KEY = '+uj8l**58(=^kg%1@x^#%vkqy3%3r883&^6azv59-z+6w%(+8('
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG

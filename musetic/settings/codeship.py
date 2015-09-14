2e6573ada02b32b4ae4ce8fb9d2e00999cb91568
joshchandler88 <joshchandler88@gmail.com>
2015-09-14 09:02:23 -0400

    Static and Media settings for codeship

--- musetic/settings/codeship.py
+++ musetic/settings/codeship.py
@@ -12,7 +12,26 @@ TEMPLATE_DEBUG = DEBUG
 
 SITE_ID = 1
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(PROJECT_ROOT, 'tmp')
+DEFAULT_FROM_EMAIL = 'no-reply@musetic.com'
+
+# REST FRAMEWORK
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+}
+
+# CACHING
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.RedisCache',
+        'LOCATION': 'redis://localhost:6379',
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
@@ -26,7 +45,22 @@ DATABASES = {
         }
 }
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+# STATIC AND MEDIA
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PUBLIC_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
+DEFAULT_S3_PATH = 'media'
+AWS_ACCESS_KEY_ID = get_env_variable('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY  = get_env_variable('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = get_env_variable('AWS_STORAGE_BUCKET_NAME')
+
+MEDIA_URL = '//s3.amazonaws.com/%s/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+
